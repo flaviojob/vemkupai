6edb0a9baca2437c2ff26e620bcbb657630b313e
Vinicius <viniciusca2@gmail.com>
2017-11-16 11:57:49 -0200

    vai curinthia

--- atividade/atividadeteste.sh
+++ atividade/atividadeteste.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "vcs sao zicka"
 echo "vemkupai que e sucesso"
+echo "sucesso e o corinthians heptacampeao, vai corinthians"
+echo "é 2x1 caraio"
+echo "dupla penetriz no flavião"
 
